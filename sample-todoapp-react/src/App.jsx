42f2eb2cfe57331f6f96d62fb71906005f3be8ee
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-16 15:11:26 +0900

    Add: ToDoリストの文字のスタイル

--- sample-todoapp-react/src/App.jsx
+++ sample-todoapp-react/src/App.jsx
@@ -6,25 +6,25 @@ export const App = () => {
       <div>
         <input type="text" />
         <button className="button large add">追加</button>
-        <h2>TODOリスト</h2>
+        <h2 className="title">TODOリスト</h2>
         <ul>
           <li>
-            <p>図書館に行く</p>
+            <p className="todoText">図書館に行く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
           </li>
           <li>
-            <p>5km歩く</p>
+            <p className="todoText">5km歩く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
           </li>
         </ul>
       </div>
       <div>
-        <h2>DONE</h2>
+        <h2 className="title">DONE</h2>
         <ul>
           <li>
-            <p>野菜を食べる</p>
+            <p className="todoText">野菜を食べる</p>
             <button className="button small back">戻す</button>
           </li>
         </ul>
