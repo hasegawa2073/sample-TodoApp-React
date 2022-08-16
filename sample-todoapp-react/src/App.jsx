e10b8ef571816300c05365ab250dcf744bc93325
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-16 15:58:38 +0900

    Add: スタイルとレイアウト完成

--- sample-todoapp-react/src/App.jsx
+++ sample-todoapp-react/src/App.jsx
@@ -10,12 +10,12 @@ export const App = () => {
         </div>
         <h2 className="title">TODOリスト</h2>
         <ul>
-          <li>
+          <li className="list">
             <p className="todo-text">図書館に行く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
           </li>
-          <li>
+          <li className="list">
             <p className="todo-text">5km歩く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
@@ -25,7 +25,7 @@ export const App = () => {
       <section className="done-area">
         <h2 className="title">DONE</h2>
         <ul>
-          <li>
+          <li className="list">
             <p className="todo-text">野菜を食べる</p>
             <button className="button small back">戻す</button>
           </li>
