16e7745f6eb75e158f506f25db5e4eb2063c1228
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-16 15:15:20 +0900

    Add: エリアの背景色

--- sample-todoapp-react/src/App.jsx
+++ sample-todoapp-react/src/App.jsx
@@ -3,32 +3,32 @@ import './styles.css';
 export const App = () => {
   return (
     <main>
-      <div>
+      <section className="todo-area">
         <input type="text" />
         <button className="button large add">追加</button>
         <h2 className="title">TODOリスト</h2>
         <ul>
           <li>
-            <p className="todoText">図書館に行く</p>
+            <p className="todo-text">図書館に行く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
           </li>
           <li>
-            <p className="todoText">5km歩く</p>
+            <p className="todo-text">5km歩く</p>
             <button className="button small done">完了</button>
             <button className="button small delete">削除</button>
           </li>
         </ul>
-      </div>
-      <div>
+      </section>
+      <section className="done-area">
         <h2 className="title">DONE</h2>
         <ul>
           <li>
-            <p className="todoText">野菜を食べる</p>
+            <p className="todo-text">野菜を食べる</p>
             <button className="button small back">戻す</button>
           </li>
         </ul>
-      </div>
+      </section>
     </main>
   );
 };
