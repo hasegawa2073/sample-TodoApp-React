7be02a31697966a7ea2ce4af4e0d5b6a3869415f
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-17 14:32:31 +0900

    Improve: 入力欄のスタイルをStyledComponentsで切り出した。

--- TodoApp/src/components/InputArea.jsx
+++ TodoApp/src/components/InputArea.jsx
@@ -1,9 +1,30 @@
+import styled from 'styled-components';
+
+const InputText = styled.input`
+  box-sizing: border-box;
+  width: 360px;
+  margin-right: 8px;
+  padding: 10px 16px;
+  font-family: 'Zen Maru Gothic', sans-serif;
+  color: #909090;
+  font-size: 2rem;
+  font-weight: 700;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+
+  @media (max-width: 500px) {
+    margin: 0;
+    width: 100%;
+  }
+`;
+
 export const InputArea = (props) => {
   const { inputText, onChangeInputText, onClickAdd } = props;
 
   return (
     <div className="input-area">
-      <input
+      <InputText
         className="input-text"
         type="text"
         value={inputText}
